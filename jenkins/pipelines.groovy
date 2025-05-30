e34a256162238771280d795a69997f1840e0220e
dineshsai07 <namburudineshsai@gmail.com>
2025-05-30 22:07:07 +0530

    Run cleanup-detector inside Python container

--- jenkins/pipelines.groovy
+++ jenkins/pipelines.groovy
@@ -1,18 +1,18 @@
 pipeline {
-  /*  
-   * Runs all stages inside the official Python image,
-   * which already includes python3 and pip.
-   */
   agent any
 
   stages {
     stage('Detect Resource Waste') {
       steps {
-        // Install the requests library
-        sh 'pip3 install requests'
-
-        // Run the detector against Prometheus in the Compose network
-        sh 'python3 scripts/cleanup-detector.py http://prometheus:9090 20 8'
+        script {
+          // Pull the official Python image and run the steps inside it:
+          docker.image('python:3.9-slim').inside('-u root:root -v /var/run/docker.sock:/var/run/docker.sock') {
+            sh '''
+              pip3 install requests
+              python3 scripts/cleanup-detector.py http://prometheus:9090 20 8
+            '''
+          }
+        }
       }
     }
   }
